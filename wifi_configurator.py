87ff12dbce2dd7971a72db60ae6cbbaa996b403c
J.D. Zamfirescu-Pereira <zamfire@gmail.com>
2019-11-18 08:39:19 -0800

    extended to include hostname + key

--- wifi_configurator.py
+++ wifi_configurator.py
@@ -5,6 +5,7 @@ from flash_reader import FlashReader
 from wifi.IWList import IWList
 
 CONFIG_FILE = "/etc/flashconfig-wifi.json"
+HOSTNAME_FILE = "/etc/hostname"
 INTERFACES_FILE = "/etc/network/interfaces"
 INTERFACES_FILE_TEMPLATE = os.path.dirname(os.path.realpath(__file__)) + "/wpa_supplicant.conf"
 
@@ -22,9 +23,9 @@ OPEN_TEMPLATE = """
 class CommandHandler:
     def handle_input(self, input):
         msgtype = input[0]
-        if msgtype == 1:
+        if msgtype == 0:
             self.handle_wifi(input)
-        elif msgtype == 3:
+        elif msgtype == 10:
             self.handle_command(input)
 
     def read_wifi_config(self):
@@ -58,10 +59,15 @@ class CommandHandler:
         uptext = os.popen(cstring).read()
         
         print "network should be back up!"
+        
+        cstring = "hostname --file /etc/hostname"
+        hntext = os.popen(cstring).read()
+        
+        print "hostname set!"
 
-    def set_new_wifi(self, network, password):
+    def set_new_wifi(self, network, password, hostname):
         f = open(CONFIG_FILE, 'w')
-        f.write(json.dumps({'network': network, 'password': password}))
+        f.write(json.dumps({'network': network, 'password': password, 'hostname': hostname}))
         f.close()
 
         wifiDetails = None
@@ -77,19 +83,47 @@ class CommandHandler:
             f = open(INTERFACES_FILE, "w")
             f.write(template % (wifiDetails))
             f.close()
+            if len(hostname) > 0:
+              f = open(HOSTNAME_FILE, "w")
+              f.write(hostname + "\n")
+              f.close()
             self.reload_wifi()
 
-    def update_wifi(self, network, password):
-        print "setting wifi with", network, password
-        self.set_new_wifi(network, password)
+    def confirm_up(self, key):
+        num_tries = 10
+        while num_tries > 0:
+          num_tries -= 1
+          cstring = "hostname -I"
+          iptext = os.popen(cstring).read()
+          iplist = [ip for ip in iptext.split(' ') if len(ip) < 16]
+          if len(iplist) > 0:
+            cstring = "curl --connect-timeout 3 -X POST http://stds.zamfi.net/%s?%s" % (key, "IP%20"+'%20'.join(iplist))
+            confirmtext = os.popen(cstring).read()
+            if confirmtext.strip() == 'ok':
+              return
+          time.sleep(5)
+
+    def update_wifi(self, network, password, hostname, key):
+        print "setting wifi with", network, password, hostname, key
+        self.set_new_wifi(network, password, hostname)
+        self.confirm_up(key)
             
     def handle_wifi(self, input):
         print "handling wifi on", input
         networkLen = input[1]
-        network = bytearray(input[2:networkLen+2]).decode('utf-8')
-        passwordLen = input[networkLen+3]
-        password = bytearray(input[networkLen+4:networkLen+4+passwordLen]).decode('utf-8')
-        self.update_wifi(network, password)
+        networkIndex = 2
+        network = bytearray(input[networkIndex:networkIndex+networkLen]).decode('utf-8').strip()
+        passwordLen = input[networkIndex+networkLen+1]
+        passwordIndex = networkIndex+networkLen+2
+        password = bytearray(input[passwordIndex:passwordIndex+passwordLen]).decode('utf-8').strip()
+        hostnameLen = input[passwordIndex+passwordLen+1]
+        hostnameIndex = passwordIndex+passwordLen+2
+        hostname = bytearray(input[hostnameIndex:hostnameIndex+hostnameLen]).decode('utf-8').strip()
+        keyLen = input[hostnameIndex+hostnameLen+1]
+        keyIndex = hostnameIndex+hostnameLen+2
+        key = bytearray(input[keyIndex:keyInedx+keyLen]).decode('utf-8')
+
+        self.update_wifi(network, password, hostname, key)
         
 
     def handle_command(self, input):
